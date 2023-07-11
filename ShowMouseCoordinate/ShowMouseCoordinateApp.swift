418f68754e05f0653399aac9362b306f528be2c8
Arnaud Lefort <zlitus@gmail.com>
2023-07-12 00:08:52 +0200

    Initial Commit

---
+++ ShowMouseCoordinate/ShowMouseCoordinateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMouseCoordinateApp.swift
+//  ShowMouseCoordinate
+//
+//  Created by Arnaud Lefort on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShowMouseCoordinateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
